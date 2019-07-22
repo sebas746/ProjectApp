c179c0b07b18c58c1436262a1b38aa2321ffe2ff
sebas746 <sebas746@gmail.com>
2019-07-21 22:19:39 -0500

    Policy context table added.

---
+++ WebApi/WebApp.DataContext/WebApp/Policy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApp.DataContext.WebApp
+{
+    public class Policy
+    {
+        public int PolicyId { get; set; }
+        public string PolicyName { get; set; }
+        public string PolicyDescription { get; set; }
+        public int PolicyPeriod { get; set; }
+        public long PolicyPrice { get; set; }
+        public DateTime PolicyStartDate { get; set; }
+        public int RiskTypeId { get; set; }
+        public int CoverageTypeId { get; set; }
+}
+}
